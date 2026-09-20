ad5d0a19ac037e7445f7a8d99c99fc7bd9e15349
danmulat <61710372+danmulat@users.noreply.github.com>
2026-09-20 08:45:40 +0300

    Export new SOC and GLEAM interfaces

--- src/open_dmrv/models/__init__.py
+++ src/open_dmrv/models/__init__.py
@@ -2,10 +2,43 @@
 
 from .biomass import woody_biomass_stock_co2e
 from .energy import farm_energy_co2e
+from .gleam_adapter import (
+    GLEAM_PINNED_COMMIT,
+    GLEAM_REPOSITORY,
+    GleamEmissionBundle,
+    GleamHerdResult,
+    GleamProductionBundle,
+    aggregate_herds,
+)
 from .livestock import enteric_methane
 from .manure import manure_emissions
 from .rothc import RothCInputs, RothCModel
-from .soc_stock import soc_stock_t_c_ha
+from .soc_stock import (
+    VM0042_MINIMUM_SOC_DEPTH_CM,
+    SoilLayer,
+    aggregate_soc_layers_t_c_ha,
+    soc_stock_t_c_ha,
+    validate_vm0042_depth,
+)
 from .soil_ghg import soil_n2o_emissions
 
-__all__ = ["RothCInputs", "RothCModel", "enteric_methane", "farm_energy_co2e", "manure_emissions", "soc_stock_t_c_ha", "soil_n2o_emissions", "woody_biomass_stock_co2e"]
+__all__ = [
+    "GLEAM_PINNED_COMMIT",
+    "GLEAM_REPOSITORY",
+    "GleamEmissionBundle",
+    "GleamHerdResult",
+    "GleamProductionBundle",
+    "RothCInputs",
+    "RothCModel",
+    "SoilLayer",
+    "VM0042_MINIMUM_SOC_DEPTH_CM",
+    "aggregate_herds",
+    "aggregate_soc_layers_t_c_ha",
+    "enteric_methane",
+    "farm_energy_co2e",
+    "manure_emissions",
+    "soc_stock_t_c_ha",
+    "soil_n2o_emissions",
+    "validate_vm0042_depth",
+    "woody_biomass_stock_co2e",
+]
